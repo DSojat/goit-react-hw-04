378956f16185ad099cd8359f7bac8cb7435f2422
DSojat <145054801+DSojat@users.noreply.github.com>
2024-04-21 23:15:35 +0300

    page func

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,61 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import SearchBar from './SearchBar/SearchBar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import Loader from './Loader/Loader';
+import ErrorMessage from './ErrorMessage/ErrorMessage';
+import LoadMoreBtn from './LoadMoreBtn/LoadMoreBtn';
+
+import getGallerySearch from '../pixabay-api';
+import { limitPage } from '../pixabay-api';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [images, setImages] = useState([]);
+  const [topicValue, setTopic] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [loadMore, setLoadMore] = useState(false);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+
+  useEffect(() => {
+    page > totalPages ? setLoadMore(false) : setLoadMore(true);
+  }, [totalPages]);
+
+  const handleSearch = async topic => {
+    try {
+      setLoading(true);
+      setImages([]);
+      setTopic(topic);
+      setPage(1);
+      const data = await getGallerySearch(topic, page);
+      setTotalPages(Math.ceil(data.totalHits / limitPage));
+      setImages(data.hits);
+    } catch (error) {
+      setError(true);
+    } finally {
+      setPage(page + 1);
+      setLoading(false);
+    }
+  };
+
+  const handleLoadMore = async () => {
+    setPage(page + 1);
+    const data = await getGallerySearch(topicValue, page);
+    setImages(data.hits);
+    if (page >= totalPages) {
+      setLoadMore(false);
+    }
+  };
 
   return (
     <>
-      <SearchBar onSubmit={onSubmit}></SearchBar>
+      <SearchBar onSubmit={handleSearch}></SearchBar>
+      <section className="gallerySection">
+        {loading && <Loader></Loader>}
+        {error && <ErrorMessage></ErrorMessage>}
+        {images.length > 0 && <ImageGallery items={images} />}
+        {loadMore && <LoadMoreBtn onClick={handleLoadMore}></LoadMoreBtn>}
+      </section>
     </>
   );
 }
---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,18 @@
+import ImageCard from '../ImageCard/ImageCard';
+import css from './ImageGallery.module.css';
+
+const ImageGallery = ({ items }) => {
+  // console.log(items);
+
+  return (
+    <ul className={css.galleryList}>
+      {items.map(({ id, webformatURL, tags }) => (
+        <li className={css.galleryItem} key={id}>
+          <ImageCard src={webformatURL} alt={tags}></ImageCard>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ImageGallery;
